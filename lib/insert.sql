36de3cb295c09aa84105b613d236c79f0473b0f5
Emily <emelizadaniels@gmail.com>
2022-11-25 15:56:45 -0500

    commit tree fd14585ae7216da0a25cc6504e43187e28ff49a9

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocololate', 4, 'M', 'brown', 'stern', FALSE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 5, 'M', 'grey', 'rowdy', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 16, 'F', 'brown', 'sweet', FALSE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 10, 'M', 'black', 'serious', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 6, 'F', 'brown', 'aloof', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 17, 'M', 'green', 'jealous', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 4, 'M', 'white', 'sexy', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 7, 'F', 'tan', 'kinda dumb, ngl', TRUE);
